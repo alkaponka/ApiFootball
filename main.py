ee09fe3645069db3fcd178e3f54f949beefaadd6
alkaponka <alexfutruke@gmail.com>
2022-06-10 12:33:28 +0300

    start

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import requests
 
-url = "https://api-football-v1.p.rapidapi.com/v3/leagues?code=gb"
 
-headers = {
-	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-	"X-RapidAPI-Key": "d26fee3105msh10f5ca34bebfe21p11bf79jsnbd00c854e495"
-}
+def country():    #список країн
+	url = "https://api-football-v1.p.rapidapi.com/v3/teams/countries"
+	headers = {
+		"X-RapidAPI-Key": "",
+		"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+	}
+	response1 = requests.request("GET", url, headers=headers)
+	if response1 == 200:
+		return print(response1.text)
+	else:
+		print(f"Error country {response1.status_code}")
 
-response = requests.request("POST", url, headers=headers)
 
-print(response.text)
+def leagues(q: str):    #список ліг
+	url = "https://api-football-v1.p.rapidapi.com/v3/leagues"
+	querystring = {"country": q}
+	headers = {
+		"X-RapidAPI-Key": "",
+		"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+	}
+	response = requests.request("GET", url, headers=headers, params=querystring)
+	if response.status_code == 200:
+		return print(response.text)
+	else:
+		print(f"error leagues {response.status_code}")
+
+def main():
+	country()
+	q = str(input("Ведіть країну щоб отримати список ліг:"))
+	leagues(q)
+
+if __name__ == '__main__':
+	main()
